f8196b071375d430b1c9080ea41b578a23f9303f
kevinnicky <kevinnickysetiawan@gmail.com>
2020-09-16 19:48:45 +0700

    add something special

--- index.js
+++ index.js
@@ -2,27 +2,87 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const yup = require('yup')
+const monk = require('monk')
+const { nanoid } = require('nanoid')
+
+require('dotenv').config()
+
+const db = monk(process.env.MONGO_URI)
+const urls = db.get('urls')
+urls.createIndex({ slug: 1}, { unique: true })
 
 const app = express()
+app.use(helmet({
+    contentSecurityPolicy: false
+}
+))
+// app.use(helmet.contentSecurityPolicy({
+//     directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc:["'self'", "cdn.jsdelivr.net"]
+//     }
+// }))
 
-app.use(helmet())
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
 app.use(express.static('./public'))
 
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required()
+})
+
 app.get('/url/:id', (req, res) => {
     // TODO: get a short url by id
 })
 
-app.get('/:id', (req, res) => {
-    // TODO: redirect to url
+app.get('/:id', async (req, res, next) => {
+    const { id: slug } = req.params
+    try {
+        const url = await urls.findOne({ slug })
+        if (url) res.redirect(url.url)
+        res.redirect(`/?error=${slug} not found`)
+    } catch (error) {
+        res.redirect('/?error=Link not found')
+    }
 })
 
-app.post('/url', (req, res) => {
-    // TODO: create a short url
+app.post('/url', async (req, res, next) => {
+    let { slug, url } = req.body
+
+    try {
+        await schema.validate({
+            slug,
+            url
+        })
+
+        if (!slug) slug = nanoid(5)
+        
+        slug = slug.toLowerCase()
+        const newUrl = {
+            url,
+            slug,
+        }
+
+        const created = await urls.insert(newUrl)
+        res.json(created)
+    } catch (error) {
+        if (error.message.startsWith('E11000')) error.message = 'Slug is in use :('
+        next(error)
+    }
 })
 
+app.use((error, req, res, next) => {
+    if (error.status) res.status(error.status)
+    else res.status(500)
+
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? "Aww snap :(" : error.stack
+    })
+})
 
 const port = process.env.PORT || 1337
 
